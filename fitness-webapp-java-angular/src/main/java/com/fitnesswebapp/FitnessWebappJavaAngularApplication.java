aa6837e541db3f7da58d5068e0e54821e181a250
Crystiane Meira <10555804+crysmeira@users.noreply.github.com>
2019-11-20 22:25:55 -0800

    Initial commit

---
+++ fitness-webapp-java-angular/src/main/java/com/fitnesswebapp/FitnessWebappJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessWebappJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessWebappJavaAngularApplication.class, args);
+	}
+
+}
---
+++ fitness-webapp-java-angular/src/test/java/com/fitnesswebapp/FitnessWebappJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWebappJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
