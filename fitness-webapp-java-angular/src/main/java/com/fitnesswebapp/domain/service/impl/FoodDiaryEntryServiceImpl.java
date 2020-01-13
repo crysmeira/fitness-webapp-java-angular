1999eb76a7a5bdfeca26af1932110926285c78fa
Crystiane Meira <10555804+crysmeira@users.noreply.github.com>
2020-01-12 16:44:23 -0800

    Removed unused imports

--- fitness-webapp-java-angular/src/main/java/com/fitnesswebapp/domain/service/impl/FoodDiaryEntryServiceImpl.java
+++ fitness-webapp-java-angular/src/main/java/com/fitnesswebapp/domain/service/impl/FoodDiaryEntryServiceImpl.java
@@ -3,7 +3,6 @@ package com.fitnesswebapp.domain.service.impl;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
